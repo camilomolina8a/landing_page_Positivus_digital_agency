6e3ef7dda42a7f72a99b5b206265c287eb980c16
Camilo Molina <camilomolina.dev@gmail.com>
2023-09-25 16:05:18 -0500

    README.md

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Hero from "./Hero";
 import NavBar from "./NavBar";
-import Companies from "./companies";
+import Companies from "./Companies";
 import Services from "./Services";
 import CTABanner from "./CTABanner";
 import CaseStudies from "./CaseStudies";
